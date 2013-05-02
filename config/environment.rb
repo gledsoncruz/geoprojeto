789871feb1810ecef496dadbc99505ef6a0e6a8a
gledson.cruz <gledson.cruz@gmail.com>
2013-05-02 13:26:23 -0300

    Corrigindo erro de timeout

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Geo::Application.initialize!
 
-config.gem "activerecord-oracle_enhanced-adapter", :lib => "active_record/connection_adapters/oracle_enhanced_adapter"
+
 Time::DATE_FORMATS[:data_br] = "%d/%m/%Y"
 Time::DATE_FORMATS[:dia_semana] = "%A"
 Time::DATE_FORMATS[:hora] = "%H:%M:%S"
