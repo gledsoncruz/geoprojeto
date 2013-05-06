99bd3bf122db5b9872521bf00891738e5ed8d331
gledson.cruz <gledson.cruz@gmail.com>
2013-05-06 11:11:54 -0300

    Corrigindo timeout

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,13 @@ if defined?(Bundler)
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
+  gem "activerecord-oracle_enhanced-adapter"
 end
 
 module Geo
   class Application < Rails::Application
 
-    config.gem "activerecord-oracle_enhanced-adapter", :git => "git://github.com/rsim/oracle-enhanced.git"
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
