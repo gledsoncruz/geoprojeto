c58c9aaf6cda27672b329812af61e1e02fbdd036
gledson.cruz <gledson.cruz@gmail.com>
2013-05-06 11:06:22 -0300

    Corrigindo timeout

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ end
 
 module Geo
   class Application < Rails::Application
+
+    config.gem "activerecord-oracle_enhanced-adapter", :git => "git://github.com/rsim/oracle-enhanced.git"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
